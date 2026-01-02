4d4884cf698ed8e3b1ef36907418dde57c98adfd
emiv9200 <emiv.9200@gmail.com>
2026-01-02 18:52:08 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,16 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    DATABASE_URL = os.getenv("DATABASE_URL")
-    
     DEBUG = os.getenv("DEBUG", "False").lower() == "true"
-    SECRET_KEY = os.getenv("SECRET_KEY", "instagram-secret-key-2025")
-    
-    TWITTER_API_KEY = os.getenv("TWITTER_API_KEY")
-    TWITTER_API_SECRET = os.getenv("TWITTER_API_SECRET")
-    TWITTER_ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN")
-    TWITTER_ACCESS_SECRET = os.getenv("TWITTER_ACCESS_SECRET")
-    TWITTER_BEARER_TOKEN = os.getenv("TWITTER_BEARER_TOKEN")
+    SECRET_KEY = os.getenv("SECRET_KEY", "instagram-secret-2025")
     
     INSTAGRAM_USERNAME = os.getenv("INSTAGRAM_USERNAME")
     INSTAGRAM_PASSWORD = os.getenv("INSTAGRAM_PASSWORD")
